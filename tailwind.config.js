fe79ac92ff2a2c9daf1d2fb8cde0100e613f928f
Surya <surya.nata.aardhana@gmail.com>
2024-06-21 01:18:10 +0700

    chore: make folder structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'primary': "#241B35",
+      }
+    },
   },
   plugins: [],
 }
