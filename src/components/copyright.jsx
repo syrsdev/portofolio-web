38a77c7ced303829b5e1e1ffd0f6aba87d4d9226
Surya <surya.nata.aardhana@gmail.com>
2024-06-27 22:19:24 +0700

    style: copyright

---
+++ src/components/copyright.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Container from "../layouts/container";
+import { FaSpotify, FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+
+function Copyright() {
+    return (
+        <Container>
+            <div className="flex justify-between items-center border-t-2 border-primary py-[30px]">
+                <div className="flex gap-5 w-max">
+                    <a href="">
+                        <FaSpotify className="w-[25px] h-[25px]" />
+                    </a>
+                    <a href="">
+                        <FaGithub className="w-[25px] h-[25px]" />
+                    </a>
+                    <a href="">
+                        <FaLinkedin className="w-[25px] h-[25px]" />
+                    </a>
+                    <a href="">
+                        <FaInstagram className="w-[25px] h-[25px]" />
+                    </a>
+                </div>
+                <div className="w-max font-bold text-[12px]">
+                    ©2024 Surya Nata Ardhana | All rights reserved
+                </div>
+            </div>
+        </Container>
+    );
+}
+
+export default Copyright;
