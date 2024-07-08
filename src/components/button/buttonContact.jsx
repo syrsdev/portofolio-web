93341c8101fefcc75ce98d4f8264292a94d312f1
Surya <surya.nata.aardhana@gmail.com>
2024-07-08 21:53:25 +0700

    style: contacts page

---
+++ src/components/button/buttonContact.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function ButtonContact({
+    children,
+    color = "bg-gradient-to-r from-[#6C35DE] to-[#A364FF]",
+    onClick,
+    href = null,
+    sm = false,
+}) {
+    return (
+        <a
+            href={href}
+            className={`px-12 text-center cursor-pointer text-[16px] flex items-center justify-center gap-2 font-bold text-white ${color} rounded-[10px] hover:shadow-[7px_5px_56px_-14px_#A364FF] transition-all duration-300 active:scale-90 active:shadow-[7px_5px_56px_-10px_#A364FF] w-full text-nowrap py-3`}
+        >
+            {children}
+        </a>
+    );
+}
+
+export default ButtonContact;
--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Container from "../layouts/container";
 import Layout from "../layouts/layout";
 import Title from "../components/title/title";
+import ButtonContact from "../components/button/buttonContact";
+import { FaGithub, FaInstagram, FaLinkedin, FaWhatsapp } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
 
 function Contacts() {
     return (
@@ -21,14 +24,48 @@ function Contacts() {
                 <div className="flex flex-col justify-between gap-10 md:flex-row">
                     <div className="flex flex-col gap-10">
                         <div className="flex flex-col">
-                            <h3 className="text-[24px] font-semibold">
+                            <h3 className="text-[18px] md:text-[22px] lg:text-[24px] font-semibold">
                                 Send me a message
                             </h3>
+                            <div className="flex flex-col w-full gap-5 mt-5 md:gap-3 lg:gap-5 md:w-full md:flex-row">
+                                <ButtonContact color="bg-red-500">
+                                    <span className="text-[20px]">
+                                        <MdOutlineEmail />
+                                    </span>
+                                    Email
+                                </ButtonContact>
+                                <ButtonContact color="bg-[#00B944]">
+                                    <span className="text-[20px]">
+                                        <FaWhatsapp />
+                                    </span>
+                                    WhatsApp
+                                </ButtonContact>
+                            </div>
                         </div>
                         <div className="flex flex-col">
-                            <h3 className="text-[24px] font-semibold">
+                            <h3 className="text-[18px] md:text-[22px] lg:text-[24px] font-semibold">
                                 Or find me on social media
                             </h3>
+                            <div className="flex flex-col w-full gap-5 my-5 md:gap-3 lg:gap-5 md:w-full md:flex-row">
+                                <ButtonContact color="bg-slate-500">
+                                    <span className="text-[20px]">
+                                        <FaGithub />
+                                    </span>
+                                    Github
+                                </ButtonContact>
+                                <ButtonContact color="bg-pink-600">
+                                    <span className="text-[20px]">
+                                        <FaInstagram />
+                                    </span>
+                                    Instagram
+                                </ButtonContact>
+                            </div>
+                            <ButtonContact color="bg-blue-600">
+                                <span className="text-[20px]">
+                                    <FaLinkedin />
+                                </span>
+                                Linkedin
+                            </ButtonContact>
                         </div>
                     </div>
                     <div className="flex justify-center w-full lg:justify-end xl:justify-center md:w-1/3 lg:w-1/2">
