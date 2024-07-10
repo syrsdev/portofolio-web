ad31589106a4445795168a408e2fa3c6cd3210ca
Surya <surya.nata.aardhana@gmail.com>
2024-07-10 23:36:16 +0700

    style: add layout on 4040 and under construction

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Layout from "../layouts/layout";
 
 function NotFound() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen text-center">
-            <img
-                src="./assets/404.svg"
-                alt="404 not found"
-                className="w-[230px] md:w-[280px]"
-            />
-            <h1 className="font-bold text-[24px] mt-8 mb-2">
-                Oops! We can’t find that page.
-            </h1>
-            <Link to={"/"} className="text-primary">
-                wanna back to home?
-            </Link>
-        </div>
+        <Layout>
+            <div className="flex flex-col items-center justify-center min-h-screen text-center">
+                <img
+                    src="./assets/404.svg"
+                    alt="404 not found"
+                    className="w-[230px] md:w-[280px]"
+                />
+                <h1 className="font-bold text-[24px] mt-8 mb-2">
+                    Oops! We can’t find that page.
+                </h1>
+                <Link to={"/"} className="text-primary">
+                    wanna back to home?
+                </Link>
+            </div>
+        </Layout>
     );
 }
 
--- src/pages/construction.jsx
+++ src/pages/construction.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Layout from "../layouts/layout";
 
 function Construction() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen gap-5">
-            <img
-                src="./assets/construc.svg"
-                alt="illustration"
-                width={"250px"}
-            />
-            <h1 className="font-bold text-[20px]">
-                Pages Under Construction, Please comeback later.
-            </h1>
-            <Link className="text-primary" to="/">
-                Back to home
-            </Link>
-        </div>
+        <Layout>
+            <div className="flex flex-col items-center justify-center min-h-screen gap-5">
+                <img
+                    src="./assets/construc.svg"
+                    alt="illustration"
+                    width={"250px"}
+                />
+                <h1 className="font-bold text-[20px]">
+                    Pages Under Construction, Please comeback later.
+                </h1>
+                <Link className="text-primary" to="/">
+                    Back to home
+                </Link>
+            </div>
+        </Layout>
     );
 }
 
