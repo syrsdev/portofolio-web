6b3a5f51708b44e318376b4b5362d3e80be684ae
Surya <surya.nata.aardhana@gmail.com>
2024-07-29 17:19:23 +0700

    style: title skeleton

---
+++ src/components/skeleton/titleSkeleton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TitleSkeleton({ width }) {
+    return (
+        <div className="w-full h-2 rounded animate-pulse bg-slate-200"></div>
+    );
+}
+
+export default TitleSkeleton;
