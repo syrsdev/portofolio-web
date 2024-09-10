010bd95828ff506133811796a12c8d4135b42d0b
Surya <surya.nata.aardhana@gmail.com>
2024-09-11 01:31:45 +0700

    style: education in about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -55,11 +55,33 @@ function About() {
                                     />
                                     <ol className="flex flex-col gap-12 pt-3 pb-6 border-l-2 border-primary">
                                         {about.education.map((item) => (
-                                            <li className="px-5">
+                                            <li className="px-5" key={item.id}>
                                                 <div className="flex items-center gap-4 mb-3">
                                                     <span className="w-3 h-3 bg-white rounded-full -ms-[26.5px]"></span>
                                                     <div className="px-2 py-2 font-bold text-sm rounded-[8px] bg-tertiary">
-                                                        june 2021 - mei 2024
+                                                        {new Date(
+                                                            item.start_date
+                                                        ).toLocaleDateString(
+                                                            "en-US",
+                                                            {
+                                                                month: "long",
+                                                                year: "numeric",
+                                                                day: "numeric",
+                                                            }
+                                                        )}{" "}
+                                                        -{" "}
+                                                        {item.end_date == null
+                                                            ? "Present"
+                                                            : new Date(
+                                                                  item.end_date
+                                                              ).toLocaleDateString(
+                                                                  "en-US",
+                                                                  {
+                                                                      month: "long",
+                                                                      year: "numeric",
+                                                                      day: "numeric",
+                                                                  }
+                                                              )}
                                                     </div>
                                                 </div>
                                                 <div className="flex flex-col gap-1">
@@ -67,7 +89,7 @@ function About() {
                                                         {item.name}
                                                     </h3>
                                                     <p className="text-[12px]">
-                                                        Software engineering
+                                                        {item.major}
                                                     </p>
                                                 </div>
                                             </li>
