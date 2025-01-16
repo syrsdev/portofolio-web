e18086f30f8698717dbdc2f8308109cb42517263
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-01-16 21:32:29 +0700

    feat: increase width n heigth of arrow

--- src/components/button/buttonToTop.jsx
+++ src/components/button/buttonToTop.jsx
@@ -6,8 +6,8 @@ const ButtonToTop = () => {
         <ScrollToTop
             smooth
             color="#fff"
-            width="15"
-            height="15"
+            width="17"
+            height="17"
             viewBox="0 0 256 256"
             className="bg-gradient-to-r from-[#6C35DE] to-[#A364FF] w-[40px] h-[40px] rounded-full flex items-center justify-center font-bold !bottom-28 !right-5 md:!right-10 md:!bottom-10 !z-[100]"
         />
