5ec492bdfa9552e932bb6fb8e8bfcb3564a82622
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-03-23 12:56:15 +0700

    fix: conditional rendering

--- src/components/copyright.jsx
+++ src/components/copyright.jsx
@@ -9,21 +9,26 @@ function Copyright() {
         <Container>
             <div className="flex pb-28 md:pb-7 justify-between flex-col md:flex-row gap-5 items-center border-t-2 border-primary py-[30px]">
                 <div className="flex gap-5 w-max">
-                    <a target="_blank" href={data == null ? "" : data.spotify}>
-                        <FaSpotify className="w-[25px] h-[25px]" />
-                    </a>
-                    <a target="_blank" href={data == null ? "" : data.github}>
-                        <FaGithub className="w-[25px] h-[25px]" />
-                    </a>
-                    <a target="_blank" href={data == null ? "" : data.linkedin}>
-                        <FaLinkedin className="w-[25px] h-[25px]" />
-                    </a>
-                    <a
-                        target="_blank"
-                        href={data == null ? "" : data.instagram}
-                    >
-                        <FaInstagram className="w-[25px] h-[25px]" />
-                    </a>
+                    {data?.spotify != null && (
+                        <a target="_blank" href={data.spotify}>
+                            <FaSpotify className="w-[25px] h-[25px]" />
+                        </a>
+                    )}
+                    {data?.github != null && (
+                        <a target="_blank" href={data.github}>
+                            <FaGithub className="w-[25px] h-[25px]" />
+                        </a>
+                    )}
+                    {data?.linkedin != null && (
+                        <a target="_blank" href={data.linkedin}>
+                            <FaLinkedin className="w-[25px] h-[25px]" />
+                        </a>
+                    )}
+                    {data?.instagram != null && (
+                        <a target="_blank" href={data.instagram}>
+                            <FaInstagram className="w-[25px] h-[25px]" />
+                        </a>
+                    )}
                 </div>
                 <div className="w-max font-semibold text-[10px] md:text-[12px]">
                     ©2024 - {new Date().getFullYear()} Surya Nata Ardhana | All
