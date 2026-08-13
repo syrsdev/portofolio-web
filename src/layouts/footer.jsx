616d787c9ac1162c94f42947121494bb8c279222
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2026-08-14 00:15:41 +0700

    style: add footer logo

--- src/layouts/footer.jsx
+++ src/layouts/footer.jsx
@@ -6,126 +6,106 @@ import { Link } from "react-router-dom";
 import { FooterData } from "../context/footerContext";
 
 function Footer() {
-    useEffect(() => {
-        document.getElementById("scroll3").onclick = () => {
-            window.scrollTo(0, 0);
-        };
-    }, []);
+  useEffect(() => {
+    document.getElementById("scroll3").onclick = () => {
+      window.scrollTo(0, 0);
+    };
+  }, []);
 
-    const { data } = useContext(FooterData);
+  const { data } = useContext(FooterData);
 
-    return (
-        <footer className="mt-[60px] border-t-2 border-primary text-wrap">
-            <Container top="pt-[50px]">
-                <div className="flex flex-col justify-between gap-12 lg:flex-row lg:gap-0">
-                    <div
-                        data-aos="fade-right"
-                        className="flex flex-col w-full gap-3 md:w-max"
-                    >
-                        <h5 className="font-bold text-[24px] ">
-                            Syrsdev Portfolio
-                        </h5>
-                        <p>
-                            Thank you for visiting my personal portfolio
-                            website, Connect with me over socials.
-                        </p>
-                    </div>
-                    <div
-                        data-aos="fade-left"
-                        className="flex lg:justify-end w-full gap-12 md:gap-[100px] lg:gap-[50px] xl:gap-[100px] flex-col md:flex-row text-wrap"
-                    >
-                        <div className="flex flex-col gap-[15px]">
-                            <h6 className="font-bold text-[20px] ">
-                                Contact Info
-                            </h6>
-                            <ul className="flex flex-col gap-[15px] text-[16px]">
-                                <li className="flex items-center gap-3">
-                                    <div>
-                                        <MdPhone className="w-[20px] h-[20px]" />
-                                    </div>
-                                    <span>
-                                        {data == null || data.telp == null
-                                            ? "..."
-                                            : "+62 " + data.telp}
-                                    </span>
-                                </li>
-                                <li className="flex items-center gap-3">
-                                    <div>
-                                        <MdEmail className="w-[20px] h-[20px]" />
-                                    </div>
-                                    <span>
-                                        {data == null || data.email == null
-                                            ? "..."
-                                            : data.email}
-                                    </span>
-                                </li>
-                                <li className="flex items-center gap-3">
-                                    <div>
-                                        <MdLocationOn className="w-[20px] h-[20px]" />
-                                    </div>
-                                    <span>
-                                        {data == null || data.address == null
-                                            ? "..."
-                                            : data.address}
-                                    </span>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="flex flex-col gap-[15px]">
-                            <h6 className="font-bold text-[20px] ">Links</h6>
-                            <ul
-                                className="flex flex-col gap-[15px]"
-                                id="scroll3"
-                            >
-                                <li>
-                                    <Link
-                                        className="hover:text-primary"
-                                        to={"/"}
-                                    >
-                                        Home
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        className="hover:text-primary"
-                                        to={"/about"}
-                                    >
-                                        About
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        className="hover:text-primary"
-                                        to={"/portfolio"}
-                                    >
-                                        Portfolio
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        className="hover:text-primary"
-                                        to={"/under-construction"}
-                                    >
-                                        Services
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        className="hover:text-primary"
-                                        to={"/contacts"}
-                                    >
-                                        Contacts
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="w-max"></div>
-                </div>
-            </Container>
-            <Copyright />
-        </footer>
-    );
+  return (
+    <footer className="mt-[60px] border-t-2 border-primary text-wrap">
+      <Container top="pt-[50px]">
+        <div className="flex flex-col justify-between gap-12 lg:flex-row lg:gap-0">
+          <div
+            data-aos="fade-right"
+            className="flex flex-col w-full gap-3 md:w-max"
+          >
+            <img src="/assets/nata-purple 2.svg" alt="syrsdev" width={56} />
+            <h5 className="font-bold text-[24px] ">Syrsdev Portfolio</h5>
+            <p>
+              Thank you for visiting my personal portfolio website, Connect with
+              me over socials.
+            </p>
+          </div>
+          <div
+            data-aos="fade-left"
+            className="flex lg:justify-end w-full gap-12 md:gap-[100px] lg:gap-[50px] xl:gap-[100px] flex-col md:flex-row text-wrap"
+          >
+            <div className="flex flex-col gap-[15px]">
+              <h6 className="font-bold text-[20px] ">Contact Info</h6>
+              <ul className="flex flex-col gap-[15px] text-[16px]">
+                <li className="flex items-center gap-3">
+                  <div>
+                    <MdPhone className="w-[20px] h-[20px]" />
+                  </div>
+                  <span>
+                    {data == null || data.telp == null
+                      ? "..."
+                      : "+62 " + data.telp}
+                  </span>
+                </li>
+                <li className="flex items-center gap-3">
+                  <div>
+                    <MdEmail className="w-[20px] h-[20px]" />
+                  </div>
+                  <span>
+                    {data == null || data.email == null ? "..." : data.email}
+                  </span>
+                </li>
+                <li className="flex items-center gap-3">
+                  <div>
+                    <MdLocationOn className="w-[20px] h-[20px]" />
+                  </div>
+                  <span>
+                    {data == null || data.address == null
+                      ? "..."
+                      : data.address}
+                  </span>
+                </li>
+              </ul>
+            </div>
+            <div className="flex flex-col gap-[15px]">
+              <h6 className="font-bold text-[20px] ">Links</h6>
+              <ul className="flex flex-col gap-[15px]" id="scroll3">
+                <li>
+                  <Link className="hover:text-primary" to={"/"}>
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link className="hover:text-primary" to={"/about"}>
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link className="hover:text-primary" to={"/portfolio"}>
+                    Portfolio
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="hover:text-primary"
+                    to={"/under-construction"}
+                  >
+                    Services
+                  </Link>
+                </li>
+                <li>
+                  <Link className="hover:text-primary" to={"/contacts"}>
+                    Contacts
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="w-max"></div>
+        </div>
+      </Container>
+      <Copyright />
+    </footer>
+  );
 }
 
 export default Footer;
